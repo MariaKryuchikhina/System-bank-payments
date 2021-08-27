2fbe8438ec00b615aef7ef63a5b0f991539e8884
mariyakryuchikhina <dino69teerex@gmail.com>
2021-08-27 15:32:53 +0300

    Изменены названия в Employee. Изменены сервисы.Добавлен интерфейс CrudService, определяющий основные методы для сервисов. Созданы имплементации сервисов для клиента и депозитного счета.

---
+++ src/main/java/com/spring/rest/system_bankpayments/service/impl/ClientServiceImpl.java
@@ -0,0 +1,13 @@
+package com.spring.rest.system_bankpayments.service;
+
+import com.spring.rest.system_bankpayments.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClientServiceImpl implements ClientService {
+
+
+}
